bc74451ada1d056e6a536d0a0c5b0c7320f68ed7
kirstinlyon <kirstin@mltwelve.com>
2025-03-29 23:00:01 +0100

    renamed function

--- R/create_competition_list.R
+++ R/create_competition_list.R
@@ -1,10 +1,10 @@
-#Title: Read event data
+#Title: Create competition list
 #AUTHOR: Kirstin Lyon
-#DESCRIPTION: Read event data
+#DESCRIPTION: Returns a list of all competitions available on the URL
 #DATE:2025-03-29
 #LICENSE: MIT
 
-#' Read event data
+#' create_competition_list
 #'
 #' @param URL URL for a competition
 #'
@@ -13,9 +13,9 @@
 #'
 #' @examples
 #'  \dontrun{
-#'    read_all_events_data(url)
+#'    create_competition_list(url)
 #' }
-read_all_events_data <- function(URL){
+create_competition_list <- function(URL){
 
     response <- httr::GET(URL)
 
