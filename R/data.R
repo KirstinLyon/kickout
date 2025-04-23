3e4e724e8c74f29eaa09af5f2bf9002ed95b57b1
kirstinlyon <kirstin@mltwelve.com>
2025-04-23 11:07:38 +0200

    Added name variation dataset and udpated begin_date to end_date for fetching competitions

--- R/data.R
+++ R/data.R
@@ -25,7 +25,20 @@
 #' \describe{
 #'   \item{data_name_club_map}{name}
 #'   \item{data_name_club_map}{country}
-#'   \item{data_name_club_map}{name_other}
 #'   \item{data_name_club_map}{club}
 #' }
 "data_name_club_map"
+
+
+#' Variable map for mapping name variations
+#'
+#' Name variations - KTK ONLY
+#'
+#' @usage data(data_name_variation_map)
+#'
+#' @format A list of the variables included to engineer dataset features
+#' \describe{
+#'   \item{data_name_club_map}{name}
+#'   \item{data_name_club_map}{name_1}
+#' }
+"data_name_variation_map"
